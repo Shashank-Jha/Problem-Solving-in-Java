2da13181a51bde129f3c579f4bd78982352a02cf
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2022-06-29 12:06:56 +0530

    Climb Stairs - Tabulation

---
+++ DP/DP2_ClimbStairs_Tabulation.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DP2_ClimbStairs_Tabulation {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
+    }
+}
